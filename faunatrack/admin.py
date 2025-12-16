2c18e5bb4ebf73893bf6f63cc11a92be73793f1c
EkawaD <steo.ederhy@gmail.com>
2025-12-16 14:26:19 +0100

    feat: clean for tp

--- faunatrack/admin.py
+++ faunatrack/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
-from faunatrack.models import Student
-
-
 # Register your models here.
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ["id", "name"]
-    list_editable = ["name"]
--- faunatrack/models.py
+++ faunatrack/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length=255)
